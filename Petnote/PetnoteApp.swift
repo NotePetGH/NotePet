ee295ea7f8bc83540b2a7e4ab8f0ec6754a83837
jmcolombini <jmccardonha@outlook.com>
2025-02-10 17:42:37 -0300

    Initial Commit

---
+++ Petnote/PetnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetnoteApp.swift
+//  Petnote
+//
+//  Created by João Marcelo Colombini Cardonha on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PetnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
